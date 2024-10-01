1c38be1b15d183f3fbf4bf3cebb3a5db8b1fdaeb
BlueCannonBall <mmttiizz100@gmail.com>
2024-10-01 19:36:53 -0400

    Clean up HN example

--- examples/hacker_news.cpp
+++ examples/hacker_news.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <memory>
 #include <nlohmann/json.hpp>
+#include <stddef.h>
 #include <stdlib.h>
 #include <vector>
 
@@ -17,10 +18,12 @@ int main() {
         return 1;
     }
     assert(resp.status_code == 200);
-
     json story_ids = json::parse(resp.body_string());
+    assert(story_ids.is_array() && story_ids.size() >= 10);
+
+    std::cout << "10 stories on Hacker News today:" << std::endl;
     std::vector<std::shared_ptr<tp::Task>> tasks;
-    for (int i = 0; i < 8; ++i) {
+    for (size_t i = 0; i < 10; ++i) {
         tasks.push_back(pw::threadpool.schedule([&story_ids, i](void*) {
             pw::HTTPResponse resp;
             if (pw::fetch("https://hacker-news.firebaseio.com/v0/item/" + std::to_string(story_ids[i].get<int>()) + ".json", resp) == PN_ERROR) {
@@ -28,13 +31,13 @@ int main() {
                 exit(1);
             }
             assert(resp.status_code == 200);
-
             json story = json::parse(resp.body_string());
+
             std::cout << story["title"].get<std::string>() << std::endl;
         }));
     }
     for (const auto& task : tasks) {
-        task->await();
+        assert(task->await() == tp::TASK_STATUS_SUCCESS);
     }
 
     pn::quit();
